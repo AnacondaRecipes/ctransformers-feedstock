1274033c5ef4cba74084bf738e8a3b00a351f189
Jean-Christophe Morin <jcmorin@anaconda.com>
2023-10-23 20:24:32 -0400

    Fix GPU support on Linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ if [[ ${target_platform} == osx-* ]]; then
     export CMAKE_ARGS="${CMAKE_ARGS} -DCT_METAL=ON"
 else
     export CMAKE_ARGS="${CMAKE_ARGS} -DCT_CUBLAS=ON"
+    conda create cuda -c nvidia/label/cuda-12.3.0 --prefix cuda_local
 fi
 
 # Notes:
@@ -12,6 +13,8 @@ fi
 # * win-64 and linux-64 supports CT_INSTRUCTIONS=avx and CT_INSTRUCTIONS=avx2. It's up to us tp decide
 #   which one we want to support.
 # * On osx, should we always enable metal? (-DCT_METAL=ON)...  THIS IS GPU! https://github.com/marella/ctransformers/blob/v0.2.27/ctransformers/lib.py#L18
+export CUDACXX=$(pwd)/cuda_local/bin/nvcc
+export CUDAToolkit_ROOT=$(pwd)/cuda_local
 cmake . -B build_ -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON ${CMAKE_ARGS}
 
 VERBOSE=1 cmake --build build_
