2b6c682c52e485599d2428c4c10b1094e098320c
Jean-Christophe Morin <jcmorin@anaconda.com>
2023-10-25 09:39:58 -0400

    Fix build script on Linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,13 @@ if [[ ${target_platform} == osx-* ]]; then
     export CMAKE_ARGS="${CMAKE_ARGS} -DCT_METAL=ON"
 else
     export CMAKE_ARGS="${CMAKE_ARGS} -DCT_CUBLAS=ON"
-    conda create cuda -c nvidia/label/cuda-12.3.0 --prefix cuda_local
+fi
+
+if [[ ${target_platform} == linux-* ]]; then
+    # Provided in docker image
+    export CUDACXX=/usr/local/cuda/bin/nvcc
+    # If this isn't included, CUDA will use the system compiler to compile host
+    export CUDAHOSTCXX="${CXX}"
 fi
 
 # Notes:
@@ -13,8 +19,6 @@ fi
 # * win-64 and linux-64 supports CT_INSTRUCTIONS=avx and CT_INSTRUCTIONS=avx2. It's up to us tp decide
 #   which one we want to support.
 # * On osx, should we always enable metal? (-DCT_METAL=ON)...  THIS IS GPU! https://github.com/marella/ctransformers/blob/v0.2.27/ctransformers/lib.py#L18
-export CUDACXX=$(pwd)/cuda_local/bin/nvcc
-export CUDAToolkit_ROOT=$(pwd)/cuda_local
 cmake . -B build_ -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON ${CMAKE_ARGS}
 
 VERBOSE=1 cmake --build build_
