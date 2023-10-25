71859820838f4dad33abaa7fdf3cd6fea38f1e06
Jean-Christophe Morin <jcmorin@anaconda.com>
2023-10-25 17:07:23 -0400

    Use multiple threads to compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,11 +18,10 @@ fi
 # * osx-arm64 and linux-aarch64 will also default to CT_INSTRUCTIONS=basic (-mcpu=native)
 # * win-64 and linux-64 supports CT_INSTRUCTIONS=avx and CT_INSTRUCTIONS=avx2. It's up to us tp decide
 #   which one we want to support.
-# * On osx, should we always enable metal? (-DCT_METAL=ON)...  THIS IS GPU! https://github.com/marella/ctransformers/blob/v0.2.27/ctransformers/lib.py#L18
-cmake . -B build_ -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON ${CMAKE_ARGS}
+cmake . -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON ${CMAKE_ARGS}
 
-VERBOSE=1 cmake --build build_
-cmake --install build_
+VERBOSE=1 cmake --build build --parallel ${CPU_COUNT}
+cmake --install build
 
 # The repo contains pre-compiled libraries. We don't want that.
 rm -v -rf ctransformers/lib
