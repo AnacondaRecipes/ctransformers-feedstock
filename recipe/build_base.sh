59ee53ed5f250b0b2af2a6fa29880bb074fc210c
Serhii Kupriienko <skupriienko@anaconda.com>
2025-01-06 12:39:09 +0200

    Enable all variants

--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -13,10 +13,6 @@ else
 fi
 
 if [[ ${target_platform} == linux-* ]]; then
-    # Provided in docker image
-    #export CUDACXX=/usr/local/cuda/bin/nvcc
-    # If this isn't included, CUDA will use the system compiler to compile host
-    #export CUDAHOSTCXX="${CXX}"
     if [[ "$target_platform" == "linux-64" ]]; then
         # See also https://github.com/marella/ctransformers/issues/120#issuecomment-1699906392
         export CMAKE_ARGS="${CMAKE_ARGS} -DCT_INSTRUCTIONS=avx -DCT_CUBLAS=${GPU_SUPPORT}"
@@ -27,7 +23,7 @@ fi
 # * osx-arm64 and linux-aarch64 will also default to CT_INSTRUCTIONS=basic (-mcpu=native)
 # * win-64 and linux-64 supports CT_INSTRUCTIONS=avx and CT_INSTRUCTIONS=avx2. It's up to us to decide
 #   which one we want to support.
-cmake . -G Ninja -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON ${CMAKE_ARGS}
+cmake . -LAH -G Ninja -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON ${CMAKE_ARGS}
 
 cmake --build build --parallel ${CPU_COUNT} --verbose
 cmake --install build
