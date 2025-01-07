d1215c2d791ee05f8cdf219d411ee03db073115c
Serhii Kupriienko <skupriienko@anaconda.com>
2025-01-07 14:04:40 +0200

    Build on win

---
+++ recipe/bld_base.bat
@@ -0,0 +1,27 @@
+@echo on
+
+SetLocal EnableDelayedExpansion
+
+if "%ctransformers_variant%"=="cpu" (
+    set "GPU_SUPPORT=OFF"
+) else (
+    set "GPU_SUPPORT=ON"
+)
+
+REM Notes:
+REM * win-64 and linux-64 supports CT_INSTRUCTIONS=avx and CT_INSTRUCTIONS=avx2. It's up to us to decide
+REM   which one we want to support.
+cmake . -G Ninja -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON %CMAKE_ARGS%
+if %ERRORLEVEL% neq 0 exit 1
+
+cmake --build build --parallel %CPU_COUNT% --verbose
+if %ERRORLEVEL% neq 0 exit 1
+
+cmake --install build
+if %ERRORLEVEL% neq 0 exit 1
+
+REM The repo contains pre-compiled libraries. We don't want that.
+rmdir /s /q ctransformers\lib
+
+set "CT_WHEEL=1"
+%PYTHON% -m pip install . -v --no-deps --no-build-isolation
